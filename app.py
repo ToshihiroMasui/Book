0a878b78f0c27f142925dae29ac2c30c0a2c1855
ToshihiroMasui <81348084+ToshihiroMasui@users.noreply.github.com>
2022-06-06 11:42:42 +0900

    test.html　テスト
    カメラが起動するか対応

--- app.py
+++ app.py
@@ -158,14 +158,39 @@ def fetch_book_data():
         return render_template('isbn.html')
 
 
+
+
+
+
+import cv2
 @app.route("/test",methods=['GET','POST'])
-def search():
-    search = request.form.get('search')
-    if not search == "":
-        if request.method == 'POST':
-            books = db.session.query(Book).filter(Book.title.contains(search)).all()
-        else:
-            books = Book.query.all()
+def camera():
+    if request.method == 'POST':
+        if request.form.get('camera') == 'camera':
+
+            #カメラの設定　デバイスIDは0
+            cap = cv2.VideoCapture(0)
+
+            #繰り返しのためのwhile文
+            while True:
+                #カメラからの画像取得
+                ret, frame = cap.read()
+
+                #カメラの画像の出力
+                cv2.imshow('camera' , frame)
+
+                #繰り返し分から抜けるためのif文
+                key =cv2.waitKey(10)
+                if key == 27:
+                    break
+
+            #メモリを解放して終了するためのコマンド
+            cap.release()
+            cv2.destroyAllWindows()
+
+            return render_template('test.html')
     else:
-        books = Book.query.all()
-    return render_template('test.html', books = books ,search = search)
+        return render_template('test.html')
+
+
+                        
